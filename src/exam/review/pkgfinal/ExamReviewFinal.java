5331e52a2d5dbab01babba75acc12fff0367b505
paterajm <134966807+paterajm@users.noreply.github.com>
2023-06-28 07:55:04 -0400

    first final review final

--- src/exam/review/pkgfinal/ExamReviewFinal.java
+++ src/exam/review/pkgfinal/ExamReviewFinal.java
@@ -9,12 +9,10 @@ package exam.review.pkgfinal;
  * @author warrior
  */
 public class ExamReviewFinal {
-
-    /**
-     * @param args the command line arguments
-     */
-    int age=10;
-    int dob=24;
+    private int dob=24;
+	private int age = 21;
+	private String fName;
+	private String lName;
     public ExamReviewFinal(){
     age=21;
     }
@@ -27,9 +25,39 @@ public class ExamReviewFinal {
     System.out.println(myObj2.age);
     System.out.println(myObj.dob);
     System.out.println(myObj2.dob);
-
+    myObj.setfName("Raj");
+    myObj2.setfName("Shrish");
+    myObj.setLName("Patel");
+myObj2.setLName("Patel");
+System.out.println("First Name: "+myObj.getfName());
+System.out.println("Last Name: "+myObj.getLName());
+System.out.println("First Name: "+myObj2.getfName());
+System.out.println("Last Name: "+myObj2.getLName());
+myObj.setLName("Patel");
+myObj2.setLName("Patel");
 
     }
+
+	public String getfName() {
+		return this.fName;
+	}
+
+	
+	public void setfName(String name) {
+		this.fName = name;
+	}
+
+	public String getLName() {
+		return this.lName;
+	}
+
+	/**
+	 * 
+	 * @param lName
+	 */
+	public void setLName(String lName) {
+		this.lName = lName;
+	}
 
     
 
