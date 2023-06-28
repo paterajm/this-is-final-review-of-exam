955db15b764bc79a56249128efd99438bcc9db01
paterajm <134966807+paterajm@users.noreply.github.com>
2023-06-28 05:57:50 -0400

    first final review

---
+++ src/exam/review/pkgfinal/ExamReviewFinal.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exam.review.pkgfinal;
+
+/**
+ *
+ * @author warrior
+ */
+public class ExamReviewFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    int age=10;
+    
+    public ExamReviewFinal(){
+    age=21;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    ExamReviewFinal myObj=new ExamReviewFinal();
+    System.out.println(myObj.age);
+    }
+    
+}
